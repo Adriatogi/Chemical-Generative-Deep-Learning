f78db8a659218acd251c8b167c713636a402528b
Ubuntu <ubuntu@ip-172-31-9-126.us-west-2.compute.internal>
2022-12-10 07:49:13 +0000

    molecule

--- molecule_generator.py
+++ molecule_generator.py
@@ -1,40 +1,76 @@
-import sys
-import os
-import pandas as pd
 import numpy as np
-import json
 from sklearn.model_selection import train_test_split
-import random as rn
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
-from numpy import array, savetxt, asarray, save
-from keras_preprocessing.sequence import pad_sequences
-from keras.models import Sequential
-from keras.layers import LSTM, Input, TimeDistributed, Dense, Activation, RepeatVector, Embedding, Dropout
-from keras.utils import np_utils
-#from google.colab import drive
-import tensorflow as tf
 from keras.preprocessing.text import Tokenizer
 from tensorflow import keras
-from tensorflow.keras.callbacks import EarlyStopping
-from keras.models import load_model
-from keras.layers import GRU
+import helper_functions as hf
 
-def generate_text(seed_text, next_words, model, max_sequence_len):
-    for _ in range(next_words):
-        token_list = Tokenizer.texts_to_sequences([seed_text])[0]
-        #token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
-        predicted = model.predict_classes(token_list, verbose=0)
-        
-        output_word = ""
-        for word,index in Tokenizer.word_index.items():
-            if index == predicted:
-                output_word = word
-                break
-        seed_text += " "+output_word
-    return seed_text.title()
+#init random seed
+seed = 1
+np.random.seed(seed)
 
-seed = '!!!!!!!!!!!!!!?'
-model = keras.models.load_model('hybrid.h5')
-generate_text(seed, 35, model, 50)
+# Filter Dataset Into Smaller Segments
+DATA_PATH = "data/data.txt"
+NEW_DATA = "data/processed_data.txt"
+TRAIN_DATA = 'data/train_data.txt'
+TEST_DATA = 'data/test_data.txt'
+VAL_DATA = 'data/val_data.txt'
+ALZHEIMERS_DATA = 'data/alzheimersdata.txt'
+
+# Filter Dataset Into Smaller Segments
+count=0
+new = open(NEW_DATA, "w")
+for line in open(DATA_PATH, "r"):
+    count += 1
+    if 30 < len(line) < 50 and count < 300000:
+        # Remove uncommon characters
+        if ('T' not in line) and ('V' not in line) and ('g' not in line) and ('L' not in line) and ('8' not in line):
+            new.write(line)
+file_new = np.array(list(open(NEW_DATA)))
+print(file_new.shape)
+
+# Access filtered data
+max_seq_len = hf.max_sequence(NEW_DATA)
+data = hf.padFile(NEW_DATA, max_seq_len) 
+data = np.array(data) 
+
+#split data into train/test
+full_train, test = train_test_split(np.array(data), test_size=0.2, random_state=seed)
+
+# split full train set into smaller train set and validation (dev) set
+train, val = train_test_split(np.array(full_train), test_size=0.10, random_state=seed)
+
+#Save the data
+np.savetxt(TRAIN_DATA, train, fmt ='%s', newline='')
+np.savetxt(TEST_DATA, test, fmt ='%s', newline='')
+np.savetxt(VAL_DATA, val, fmt='%s', newline='')
+
+tokenizer = Tokenizer(num_words=None, char_level=True, lower=False) 
+
+# Train Data Processing
+train = hf.concatenate(train) 
+x_train, y_train_2 = hf.prepare_data(train, tokenizer)
+
+#Val Data Processing
+val = hf.concatenate(val) 
+x_val, y_val_2 = hf.prepare_data(val, tokenizer)
+
+# Load the model and data
+model = keras.models.load_model('models/TL_improved_lstm.h5')
+ad_data = np.array(list(open(ALZHEIMERS_DATA)))
+print(ad_data.shape)
+smiles=[]
+
+# Create 100 molecules
+for i in range(100):
+    if i % 10 == 0:
+      print(i)
+    start = np.random.randint(0, len(ad_data)-1)
+    single_seed = ad_data[start][0:15]
+    prediction = hf.generate_text(single_seed, 35, model, 50, tokenizer)
+    print("Prediction",prediction)
+    smiles.append(prediction)
+print(smiles)
+
+# Filter molecules
+generated_molecules = hf.checkSMILES(smiles)
+print(generated_molecules)
